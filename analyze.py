14a31e85e3bcfd8368aca408a7facaf57ccfefbc
Blake Frank <blakefrank@dhcp-10-105-138-29.wireless.northwestern.private>
2023-01-14 14:43:42 -0600

    finished part G, motors

--- analyze.py
+++ analyze.py
@@ -4,9 +4,16 @@ import matplotlib.pyplot as plt
 # load the sensor values from the file
 backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
 frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+targetAngles = numpy.load("data/targetAngles.npy")
+targetAngles1 = numpy.load("data/targetAngles1.npy")
 
 # plot the sensor values
-plt.plot(backLegSensorValues, label="Back Leg Sensor", linewidth=2)
-plt.plot(frontLegSensorValues, label="Front Leg Sensor")
+# plt.plot(backLegSensorValues, label="Back Leg Sensor", linewidth=2)
+# plt.plot(frontLegSensorValues, label="Front Leg Sensor", linewidth = 2)
+plt.plot(targetAngles, label="Target Angles for Back Leg")
+plt.plot(targetAngles1, label="Target Angles for Front Leg")
+plt.title("Sensor and Target Angles Plot")
+plt.xlabel("Time Steps")
+plt.ylabel("Sensor and Target Angles Values")
 plt.legend()
 plt.show()
